0242c40a0552f85db0192ea1618ca03b4e88baa8
DEEPAK-RAMGIRI <ramgirideepak89@gmail.com>
2025-08-19 23:21:23 +0530

    updated

--- JS.js
+++ JS.js
@@ -33,7 +33,7 @@ function myMenuFunction(){
 
 /* ----- TYPING EFFECT ----- */
  var typingEffect = new Typed(".typedText",{
-    strings : ["Student","Developer"],
+    strings : ["CSE Student","Problem Solver","Fast Learner"],
     loop : true,
     typeSpeed : 100, 
     backSpeed : 80,
